dcb0426cb71f6a78a56a2f3213a2738aee0f3e80
BAS <63766016+BonnerDev@users.noreply.github.com>
2020-07-15 15:48:20 +0800

    commit 15072020

--- PyPoll/analysis/pypoll.py
+++ PyPoll/analysis/pypoll.py
@@ -3,12 +3,13 @@ import csv
 
 #set counters to 0
 row_count = 0
+winnercount = 0 
 
 #variable list
 candidate_list = []
 unique_candidates = []
-#percentage = []
-#vote = []
+percentage = []
+vote = []
 
 csvpath = os.path.join("..","resources","election_data.csv")
 with open (csvpath) as csvfile:
@@ -29,14 +30,60 @@ with open (csvpath) as csvfile:
     unique_candidates=set(candidate_list)
         #convert the set back to a list otherwise we can not iterate in a set 
     unique_candidates=list(unique_candidates)
-#print(unique_candidates[2])
-#print(row_count)
+
+#test print
+#print(len(unique_candidates)) #tested an it shows the number 4
+
+#calc total votes for each using loop through the list 
+for votes in range(0,len(unique_candidates)):
+    voting_list = candidate_list.count((unique_candidates[votes]))
+
+    #test above
+    #print(voting_list)
+
+    #put the above data into the list for vote
+    vote.append(voting_list)
+
+#find percentage of the votes for each candidate
+    percent = round(voting_list/row_count*100,3)
+
+    percentage.append(percent)
+
+    
+
+
+# Calculate the winner by looping through the unique candidate list
+for name in range(len(unique_candidates)):
+
+    if winnercount < vote[name]:
+        winner = unique_candidates[name]
+        winnercount = vote[name]
 
         
-    print("Election Results")
-    print("---------------------------------")
-    print(f"Total Votes: {row_count}")
-    print("---------------------------------")
-    
+print("")
+print("Election Results")
+print("---------------------------------")
+print(f"Total Votes: {row_count}")
+print("---------------------------------")
+print(f"{unique_candidates[0]}: {percentage[0]}% ({vote[0]})")
+print(f"{unique_candidates[1]}: {percentage[1]}% ({vote[1]})")
+print(f"{unique_candidates[2]}: {percentage[2]}% ({vote[2]})")
+print(f"{unique_candidates[3]}: {percentage[3]}% ({vote[3]})")
+print("---------------------------------")
+print(f"Winner: {winner}")
+print("")
 
-    
+#script then needs to print the analysis to terminal and export a text file
+with open('file_out.txt', mode='w') as text_file:
+    print("",file=text_file)
+    print("Election Results",file=text_file)
+    print("---------------------------------",file=text_file)
+    print(f"Total Votes: {row_count}",file=text_file)
+    print("---------------------------------",file=text_file)
+    print(f"{unique_candidates[0]}: {percentage[0]}% ({vote[0]})",file=text_file)
+    print(f"{unique_candidates[1]}: {percentage[1]}% ({vote[1]})",file=text_file)
+    print(f"{unique_candidates[2]}: {percentage[2]}% ({vote[2]})",file=text_file)
+    print(f"{unique_candidates[3]}: {percentage[3]}% ({vote[3]})",file=text_file)
+    print("---------------------------------",file=text_file)
+    print(f"Winner: {winner}",file=text_file)
+    print("",file=text_file)
---
+++ PyPoll/main.py
@@ -0,0 +1,157 @@
+# Import os module 
+import os 
+
+# Import csv module 
+import csv
+
+# Set file path
+budget_csv = os.path.join("Resources", "election_data.csv")
+
+# Set a variable for the net total amount of "Profit/Losses" over the entire period
+total_p_l = 0.00
+
+# Set a variable for the total number of months included in the dataset (exclude header row)
+total_months = 0
+
+# Create empty lists to hold the profit increase, decrease and associated dates
+profit = []
+month = []
+change = []
+
+# Set a variable for greatest increase and decrease and associated months 
+greatest_increase = 0
+greatest_increase_month = ""
+
+greatest_decrease = 0
+greatest_decrease_month = ""
+
+# Set a variable for each row total 
+rowtotal = 0
+
+# Set a variable for the first row
+firstrow = 0
+
+# Set a variable for the last row
+lastrow = 0
+
+# Set a variable for the previous row value
+previousvalue = 0 
+
+
+# Open and read csv file 
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+# Read the header row first and print to check that we have read the data correctly
+    csv_header = next(csvfile)
+    # print(f"Header: {csv_header}")
+    
+    # Read through each row of data after the header
+    for row in csvreader:
+
+        # Add profit amount to the total 
+        total_p_l = (float(row[1]) + total_p_l)
+       
+# Print to check total profit/loss is being calculated correctly
+# print(total_p_l)    
+
+# Calculate the first and last row to determine the average change
+   
+        if total_months == 0:
+            firstrow = (float(row[1]))
+
+        else: 
+            lastrow = (float(row[1])) 
+
+# Pring the first and last row        
+# print (firstrow)
+# print (lastrow)
+
+        # Add total months 
+        total_months = (total_months+1)
+
+# Print to check total months is being calculated correctly
+# print(total_months)
+        
+    # Find greatest increase or decrease in profits (amount) over the entire period
+        profit.append(float(row[1]))
+
+        change.append(float(row[1]) - previousvalue)
+    
+        previousvalue = (float(row[1]))
+
+# Find greatest increase or decrease in profits (month) over the entire period
+
+        month.append(str(row[0]))
+
+# Assign values for max and min profit variables
+
+        max_profit = (max(profit))
+        min_profit = (min(profit))
+
+# Assign values for greatest increase and decrease variables
+        greatest_increase = (max(change))
+        greatest_decrease = (min(change))
+
+# Find date of greatest increase in profits
+        if float(row[1]) == (max(profit)):
+                greatest_increase_month = (row[0])
+
+# Print max profit month to check date is correct
+# print(greatest_increase_month)
+
+# Find date of greatest decrease in progits
+        if float(row[1]) == (min(profit)):
+                greatest_decrease_month =  (row[0])
+
+# Print max profit month to check date is correct
+# print(greatest_decrease_month)
+
+# Calculate the average of the changes in "Profit/Losses" over the entire period
+average_change = (firstrow - lastrow)/(1-total_months)
+
+# Print to check average change is being calculated correctly
+# print(average_change)
+
+
+# # Print the analysis to the terminal
+
+# Print Header
+print("Financial Analysis")
+print("----------------------------")
+
+# Print total number of months
+print(f"Total Months: {total_months} months")
+
+# Print net total profit/loss  
+print(f"Total: ${total_p_l:.0f}")
+
+# Print average change in profit/loss
+print(f"Average Change: ${average_change:.2f}")
+
+# Print Greatest Profit Increase and Corresponding Date 
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase:.2f})")
+
+# Print Greatest Profit Decrease and Corresponding Date
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease:.2f})")
+
+# Export a text file with the results
+# Specify the file to write to
+output_path = os.path.join("Analysis", "Financial_Analysis.csv")
+
+# Open the file using "write mode."
+with open(output_path, 'w', newline="") as csvfile:
+
+        # Initialise csv.writer
+        csvwriter = csv.writer(csvfile, delimiter=",")
+
+        # Write the first row
+        csvwriter.writerow(["Financial Analysis"])
+
+        # Write rows 2 to 7
+        csvwriter.writerow(["----------------------------"])
+        csvwriter.writerow([f"Total Months: {total_months} months"])
+        csvwriter.writerow([f"Total: ${total_p_l:.0f}"])
+        csvwriter.writerow([f"Average Change: ${average_change:.2f}"])
+        csvwriter.writerow([f"Greatest Increase in Profits: {greatest_increase_month} (${(greatest_increase):.2f})"])
+        csvwriter.writerow([f"Greatest Decrease in Profits: {greatest_decrease_month} (${(greatest_decrease):.2f})"])
